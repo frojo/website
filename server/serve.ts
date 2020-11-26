80e1fe4991f58e0a1fc7ed19a7204301ffbe85d1
fran <franmorojo@gmail.com>
2020-11-26 15:20:02 -0500

    start server code and first build

---
+++ server/serve.ts
@@ -0,0 +1,20 @@
+import * as http from "http";
+import * as express from "express";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 6969;
+
+app.use(express.static("../docs"));
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+const handleError = (err: any, res: any) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
