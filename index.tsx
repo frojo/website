e69078ad46e1e5b6c360a1ea37f9bbbeeb041ec8
fran <franmorojo@gmail.com>
2020-11-27 13:43:26 -0500

    make render not die

--- index.tsx
+++ index.tsx
@@ -4,10 +4,10 @@ let i = 0;
 
 let lasttick = Date.now();
 function tick() {
-  console.log("tick uwu");
+  // console.log("tick uwu");
   i++;
   lasttick = Date.now();
-  render();
+  // render();
   window.requestAnimationFrame(tick);
 }
 
--- src/render.tsx
+++ src/render.tsx
@@ -2,12 +2,9 @@ import * as React from "react";
 import ReactDOM = require("react-dom");
 
 function render() {
-  const { camera, entities, me, agents, center } = getState();
-  const cameraPos = Vector.sub(center, camera);
   const element = (
     <React.Fragment>
     </React.Fragment>
   );
-  ReactDOM.render(element, document.getElementById("window"));
-  dragElement(document.getElementById("window"));
+  ReactDOM.render(element, document.getElementById("page"));
 }
