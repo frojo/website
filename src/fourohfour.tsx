63838f8ef1981e26e5d642079c95e6c6974c421a
fran <franmorojo@gmail.com>
2021-01-12 01:45:07 -0500

    build

--- src/fourohfour.tsx
+++ src/fourohfour.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 
+/* i've just learned this is a virtual 404. ok */
+
 class FourOhFour extends React.Component {
   render() {
     return (
       <div>
-        <p>404 I am sorry that I don't understand what you want.</p>
+        <p><b>404</b>. You're asking me for something but I don't understand you :(</p>
       </div>
     );}
 }
