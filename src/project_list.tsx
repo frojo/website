693be5b0d1366ad4505fcb25aead9fa62c520a3f
fran <franmorojo@gmail.com>
2021-01-12 06:18:06 +0000

    build

--- src/project_list.tsx
+++ src/project_list.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import dateFormat from "dateFormat";
+import dateFormat from "dateformat";
 
 import project_metas from "./../assets/project_metas.json"
 
